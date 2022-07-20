4d58590c31e61bf8a0704ffa5f8fa5663d080a8f
nickgautam <er.nishantgautam0011@gmail.com>
2022-07-21 00:00:48 +0530

    done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,4 +1,4 @@
-const { model } = require("mongoose")
+const mongoose = require("mongoose")
 const urlModel = require("../models/urlModel")
 const shortid = require('shortid')
 
@@ -12,17 +12,17 @@ const createUrl = async function(req,res){
                 message: "Request body can't be empty"
             })
         }
-        var urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/
+       let urlPattern = /^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/[^\s]*)?$/
  
  
    if (!urlPattern.test(data.longUrl)){
     return res.status(400).send({status: false , message: "Url is invalid"})
    }
  
-        let urlDecodedCode = shortid.generate()
-        let shortDecodedUrl = "http://localhost:3000/"+urlDecodedCode
-        data.shortUrl = shortDecodedUrl
-        data['urlCode'] = urlDecodedCode
+        let urlCode = shortid.generate()
+        let shortUrl = "http://localhost:3000/"+urlCode
+        data.shortUrl = shortUrl
+        data['urlCode'] = urlCode
         let result = await urlModel.create(data)
         return res.status(201).send({status:true, data:result})
     }
@@ -37,15 +37,12 @@ const getUrl = async function(req,res){
   try { 
     let code = req.params.urlCode
 
-    if(!shortid.isValid(code)){
-        return res.status(400).send({status:false,msg:"Invalid Code"})
-    }
     let getLongUrl = await urlModel.findOne({urlCode:code}).select({_id:0,longUrl:1})
     if(!getLongUrl){
-        return res.status(404).send({status:false,message:"No Url Found"})
+        return res.status(404).send({status:false,message:"No data found"})
     }
 
-     res.status(302).send("Redirect to " + getLongUrl.longUrl)
+     res.status(302).redirect(getLongUrl.longUrl)
 }
 catch(err){
     return res.status(500).send({status:false,message:err.message})
